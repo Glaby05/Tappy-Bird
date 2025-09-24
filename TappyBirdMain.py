2e14f56c9a83c0c12141dc878d449eebe1181e57
atmadjal <laura.atmadja@mail.utoronto.ca>
2025-09-24 09:40:41 -0400

    added Game skeleton code

--- TappyBirdMain.py
+++ TappyBirdMain.py
@@ -2,3 +2,30 @@ import pygame
 from pygame.time import get_ticks
 from pygame.mask import from_surface
 
+
+class Game:
+    """ This class represents the Game. It contains all the game objects. """
+    def __init__(self):
+        pass
+
+    def poll(self):
+        """mouse/keyboard event handler."""
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def run(self):
+        pass
+
+
+if __name__ == '__main__':
+    g = Game()
+    print("starting...")
+    g.run()
+    print("shuting down...")
+    pygame.quit()
+
