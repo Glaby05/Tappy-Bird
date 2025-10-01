d6a96e1702b3cecd13c5b76022e1cbf42d22fb30
Michael Chininea <82243647+Twilii@users.noreply.github.com>
2025-10-01 18:30:16 -0400

    Files updated

--- Objects.py
+++ Objects.py
@@ -4,12 +4,35 @@ from pygame.mask import from_surface
 
 # appropriate scaling behaviour (e.g. another level, a boss enemy)
 # life decreases faster and player moves faster overtime
+class Sprite(pygame.sprite.Sprite):
+    def __init__(self, image, startx, starty):
+        super().__init__()
 
+        self.image = pygame.image.load(image).convert_alpha()
+        self.rect = self.image.get_rect()
 
-class Player(pygame.sprite.Sprite):
+        self.rect.center = [startx, starty]
+
+    def update(self):
+        pass
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+
+class Player(Sprite):
     """objects that move over time.
     objects who move or are transformed in reaction to user activities."""
-    pass
+    def __init__(self, startx, starty, index=0):
+        super().__init__("assets/Flap.png", startx, starty)
+
+        tile_width = 64
+        tile_height = 64
+
+        x = tile_width * index
+        rect = pygame.Rect(x, 0, tile_width, tile_height)
+
+        self.image = self.image.subsurface(rect).copy()
+        self.rect = self.image.get_rect(topleft=(startx, starty))
 
 
 class Wall(pygame.sprite.Sprite):
--- TappyBirdMain.py
+++ TappyBirdMain.py
@@ -2,6 +2,9 @@ import pygame
 from pygame.time import get_ticks
 from pygame.mask import from_surface
 
+screen_info = pygame.display.Info()
+SCREEN_WIDTH = screen_info.current_w
+SCREEN_HEIGHT = screen_info.current_h
 
 class Game:
     """ This class represents the Game. It contains all the game objects. """
@@ -21,11 +24,31 @@ class Game:
     def run(self):
         pass
 
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    # Set the font for the start and game over screens
+    font = pygame.font.Font(None, 32)
+    big_font = pygame.font.Font(None, 48)
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill((255, 255, 255))
+        pygame.display.flip()
+
+    pygame.quit()
 
 if __name__ == '__main__':
-    g = Game()
-    print("starting...")
-    g.run()
-    print("shuting down...")
-    pygame.quit()
+    # g = Game()
+    # print("starting...")
+    # g.run()
+    # print("shutting down...")
+    # pygame.quit()
+    main()
 
